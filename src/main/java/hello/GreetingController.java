c7bd1eea56b2bf63605b5897e1cf25c19697d3b9
Tetiana-10 <uiuiuieuiuiui+2@yandex.ru>
2019-04-10 07:20:23 +0200

    default metrics enabled

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Properties;
 
 import org.json.JSONObject;
 import org.springframework.http.HttpHeaders;
@@ -23,6 +24,7 @@ import com.amazonaws.services.cloudwatch.AmazonCloudWatchClientBuilder;
 import com.amazonaws.services.cloudwatch.model.MetricDatum;
 import com.amazonaws.services.cloudwatch.model.PutMetricDataRequest;
 import com.amazonaws.services.cloudwatch.model.StandardUnit;
+import com.amazonaws.services.s3.model.Region;
 
 import net.sf.jasperreports.engine.JRException;
 import net.sf.jasperreports.engine.JasperCompileManager;
@@ -38,23 +40,25 @@ public class GreetingController {
 	public ResponseEntity<byte[]> generatePdf(@RequestBody String users) throws JRException, IOException {
 		File file = new File("Jasper.pdf");
 
-      final AmazonCloudWatch cw = 
-			    AmazonCloudWatchClientBuilder.standard().withRegion(Regions.EU_CENTRAL_1).build();
-		      AwsSdkMetrics.enableDefaultMetrics();
-		
-			MetricDatum datum = new MetricDatum()
-			    .withMetricName("VISITED")
-			    .withUnit(StandardUnit.None)
-			    .withValue(50.0);
+//      AmazonCloudWatch cw = 
+//			    AmazonCloudWatchClientBuilder.standard().withRegion(Regions.EU_CENTRAL_1).build();
+		AwsSdkMetrics.setRegion(Regions.EU_CENTRAL_1);
+		AwsSdkMetrics.enableDefaultMetrics();
+		System.out.println(AwsSdkMetrics.isDefaultMetricsEnabled());
 
-			PutMetricDataRequest request = new PutMetricDataRequest()
-					.withNamespace("PDFGenerator/TRAFFIC")
-					.withMetricData(datum);
-			cw.putMetricData(request);
-					
+//			MetricDatum datum = new MetricDatum()
+//			    .withMetricName("VISITED")
+//			    .withUnit(StandardUnit.None)
+//			    .withValue(50.0);
+//
+//			PutMetricDataRequest request = new PutMetricDataRequest()
+//					.withNamespace("PDFGenerator/TRAFFIC")
+//					.withMetricData(datum);
+//			cw.putMetricData(request);
+
 		JasperReport jasperReport = JasperCompileManager.compileReport("template.jrxml");
 		JSONObject jsonObj = new JSONObject(users);
-                ByteArrayInputStream jsonDataStream = new ByteArrayInputStream(jsonObj.get("users").toString().getBytes());
+		ByteArrayInputStream jsonDataStream = new ByteArrayInputStream(jsonObj.get("users").toString().getBytes());
 		JsonDataSource ds = new JsonDataSource(jsonDataStream);
 		Map<String, Object> parameters = new HashMap<String, Object>();
 		parameters.put("ReportTitle", "List of Contacts");
@@ -63,12 +67,12 @@ public class GreetingController {
 		JasperExportManager.exportReportToPdfFile(jasperPrint, "Jasper.pdf");
 		String filename = "Jasper.pdf";
 		HttpHeaders headers = new HttpHeaders();
-	    headers.setContentType(MediaType.parseMediaType("application/pdf"));
-	    
+		headers.setContentType(MediaType.parseMediaType("application/pdf"));
+
 		headers.setContentDispositionFormData(filename, filename);
-	    headers.setCacheControl("must-revalidate, post-check=0, pre-check=0");
-	    byte[] contents = (Files.readAllBytes(file.toPath()));
-	    ResponseEntity<byte[]> response = new ResponseEntity<>(contents, headers, HttpStatus.OK);
+		headers.setCacheControl("must-revalidate, post-check=0, pre-check=0");
+		byte[] contents = (Files.readAllBytes(file.toPath()));
+		ResponseEntity<byte[]> response = new ResponseEntity<>(contents, headers, HttpStatus.OK);
 		return response;
 	}
 }
